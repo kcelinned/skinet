16b7d65266c68b147a3f1360ea6cb12e8b082190
kcelinned <kc_diccion@yahoo.co.uk>
2020-07-08 22:11:05 +0100

    Initial Commit

---
+++ Core/Entities/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection.Emit;
+
+namespace Core.Entities
+{
+	public class Product
+	{
+
+		public int Id { get; set; }
+
+        public string Name { get; set; }
+		
+    }
+
+}
+
