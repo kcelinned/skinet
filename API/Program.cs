10ee52c3806ae7fbb21a4694b9b9e5f1a5dccb77
kcelinned <kc_diccion@yahoo.co.uk>
2020-07-09 22:29:27 +0100

    API Architecture

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -11,9 +11,32 @@ namespace API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            // using makes sure everything inside the statement is disposed as soon as the method is finished
+            using(var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                // LoggerFactory?
+                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+                try
+                {
+                    var context = services.GetRequiredService<StoreContext>();
+                    // applies any pending migrations to the context to the database
+                    // or creates the database if it exists 
+                    // as soon as the application starts 
+                    await context.Database.MigrateAsync();
+                    await StoreContextSeed.SeedAsync(context, loggerFactory);
+                }
+                catch(Exception ex)
+                {
+                    var logger = loggerFactory.CreateLogger<Program>();
+                    logger.LogError(ex, "An error accured during Migration"); 
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
