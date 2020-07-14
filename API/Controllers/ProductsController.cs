454eb382841a9c8264b0b35cc1016036f09226e9
kcelinned <kc_diccion@yahoo.co.uk>
2020-07-14 21:25:21 +0100

    Paging,Sorting,Filtering

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -8,6 +8,7 @@ using API.Dtos;
 using AutoMapper;
 using API.Errors;
 using Microsoft.AspNetCore.Http;
+using API.Helpers;
 
 namespace API.Controllers
 {
@@ -39,12 +40,17 @@ namespace API.Controllers
         // method to get all products using the method in the repository 
         // url is the base url : "/api/products"
         [HttpGet]
-		public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts()
+		public async Task<ActionResult<Pagination<ProductToReturnDto>>> GetProducts(
+          [FromQuery]ProductSpecParams productParams)
         {
-            var spec = new ProductsWithTypesandBrandsSpecification();
+
+            var spec = new ProductsWithTypesandBrandsSpecification(productParams);
+            var countSpec = new ProductWithFiltersForCountSpecification(productParams);
+
+            var totalItems = await _productsRepo.CountAsync(countSpec);
             var products = await _productsRepo.ListAsync(spec);
-            return Ok(
-                _mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products));
+            var data = _mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products);
+            return Ok(new Pagination<ProductToReturnDto>(productParams.PageIndex, productParams.PageSize, totalItems, data));
         }
 
 
