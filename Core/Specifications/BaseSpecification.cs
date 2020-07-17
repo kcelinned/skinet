4c9a237d2aabb7b501238a564f072e08dccf1f73
kcelinned <kc_diccion@yahoo.co.uk>
2020-07-17 19:57:35 +0100

    Navbar and API setup

--- Core/Specifications/BaseSpecification.cs
+++ Core/Specifications/BaseSpecification.cs
@@ -20,13 +20,39 @@ namespace Core.Specifications
 
         public List<Expression<Func<T, object>>> Includes { get; } =
             new List<Expression<Func<T, object>>>();
-    
+
+        public Expression<Func<T, object>> OrderBy { get; private set; }
+
+        public Expression<Func<T, object>> OrderByDescending { get; private set; }
+
+        public int Take { get; private set; }
+
+        public int Skip { get; private set; }
+
+        public bool IsPagingEnabled { get; private set; }
 
         // method to add to List of Expressions - Includes 
         // protected - method can only be accessed by this class and child classes
         protected void AddInclude(Expression<Func<T, object>> includeExpression)
         {
             Includes.Add(includeExpression);
+        }
+
+        protected void AddOrderBy(Expression<Func<T, object>> OrderByExpression)
+        {
+            OrderBy = OrderByExpression;
+        }
+
+        protected void AddOrderByDescending(Expression<Func<T, object>> OrderByDescExpression)
+        {
+            OrderByDescending = OrderByDescExpression;
+        }
+
+        protected void ApplyPaging(int skip, int take)
+        {
+            Skip = skip;
+            Take = take;
+            IsPagingEnabled = true;
         }
     }
 }
