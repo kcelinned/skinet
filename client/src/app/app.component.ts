714c1472a6ae40cd59b9ba8f748881609e43e134
kcelinned <kc_diccion@yahoo.co.uk>
2020-08-04 21:51:49 +0100

    Basket Components

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BasketService } from './basket/basket.service';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +9,20 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit{
   title = 'Skinet';
 
-  constructor() { }
+  constructor(private basketService: BasketService) { }
 
   ngOnInit(): void {
+
+    // when the app is initialised -
+    // check if there is a basketId in local storage 
+    const basketId = localStorage.getItem('basket_id');
+    if (basketId){
+      // get this basket from api and subscribe to it 
+      this.basketService.getBasket(basketId).subscribe(() => {
+        console.log('initalised basket');
+      }, error =>{
+        console.log(error);
+      });
+    }
   }
 }
